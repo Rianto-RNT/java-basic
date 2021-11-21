a2cdbbf162c42cfaeb399c863721057d5e33e8be
Rianto <rianto.rnt@gmail.com>
2021-11-21 12:05:00 +0800

    initial commit

---
+++ src/variable.java
@@ -0,0 +1,16 @@
+public class variable {
+    public static void main(String[] args) {
+
+        char var1 = '7';
+        String var2 = "my name is ryan";
+        boolean var3 = true;
+        int var4 = 90;
+        double var5 = 90.9;
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+    }
+}

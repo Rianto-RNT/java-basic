bfe2af4324a362d8fc351c656e935616852dab8b
Rianto <rianto.rnt@gmail.com>
2021-11-21 15:29:02 +0800

    Scanner Method Number And Boolean

---
+++ src/ScannerMethodNumberAndBoolean.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ScannerMethodNumberAndBoolean {
+    public static void main(String[] args) {
+
+      Scanner scan = new Scanner(System.in);
+//      double txt = scan.nextDouble(); // output: integer/number
+      boolean txt = scan.nextBoolean(); // output/input: false/true
+
+        System.out.println(txt);
+
+    }
+}

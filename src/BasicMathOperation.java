9e70184c58d55b11f0d4b510d5edff4e7d380047
Rianto <rianto.rnt@gmail.com>
2021-11-21 14:55:10 +0800

    Basic Math Operations

---
+++ src/BasicMathOperation.java
@@ -0,0 +1,27 @@
+public class BasicMathOperation {
+    public static void main(String[] args) {
+
+        double var1 = 5;
+        double var2 = 7;
+        double var3 = 10;
+        double var4 = 20;
+        double var5 = var1 + var2;
+        String var6 = "i love using";
+        int var7 = 2;
+        String var8 = "monitor when coding java";
+
+        System.out.println(var1 + var5);
+        System.out.println(var6);
+        System.out.println(var7);
+        System.out.println(var8);
+        System.out.println(var1 + var5 * var3 / var4);
+        System.out.println(var6 +" "+ var7 +" "+ var8);
+
+        System.out.println(5 + 10);
+        System.out.println(5 - 10);
+        System.out.println(5 * 10);
+        System.out.println(5 / 10);
+        System.out.println(5 % 10);
+
+    }
+}

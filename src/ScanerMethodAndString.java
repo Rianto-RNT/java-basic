d9d89b1a406215f4a5f1024564cdbf1d9708fc62
Rianto <rianto.rnt@gmail.com>
2021-11-21 15:12:36 +0800

    Scanner Method and String

---
+++ src/ScanerMethodAndString.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ScanerMethodAndString {
+    public static void main(String[] args) {
+
+//        String method1 = "hello";
+        Scanner scan = new Scanner(System.in);
+        String txt = scan.next();
+
+//        System.out.println(method1);
+        System.out.println(txt);
+
+    }
+}

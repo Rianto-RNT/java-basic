41d5ec4cc64c239ece877e55403d949bfe863773
Rianto <rianto.rnt@gmail.com>
2021-11-22 19:03:44 +0800

    Selector part 2

--- src/SelectorPart1.java
+++ src/SelectorPart1.java
@@ -8,7 +8,26 @@ public class SelectorPart1 {
         String Ans1 = scan.next();
 
         if (Ans1.equals("yes")) {
-            System.out.println("what is your favorite color between red-blue-yellow");
+            System.out.println("what is your favorite color between red-blue-yellow?");
+            String Ans2 = scan.next();
+
+            if (Ans2.equals("red")) {
+                System.out.println("we have in stock! would you like to see it. Yes or No?");
+                String Ans3 = scan.next();
+
+                if (Ans3.equals("yes")) {
+                    System.out.println("Great, let's check the laptop");
+                } else {
+                    System.out.println("No worries, we will check something else");
+                }
+
+
+            } else if (Ans2.equals("blue")) {
+                System.out.println("we can order it");
+            }
+            else {
+                System.out.println("we don't have it in stock");
+            }
         }
         else {
             System.out.println("no worries have a good day");

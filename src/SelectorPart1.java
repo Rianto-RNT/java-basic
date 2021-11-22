40c4a0dc281d041ccbb354906bc054d5113c9484
Rianto <rianto.rnt@gmail.com>
2021-11-22 18:46:26 +0800

    Selector Part 1.1

--- src/SelectorPart1.java
+++ src/SelectorPart1.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class SelectorPart1 {
+    public static void main(String[] args) {
+
+        System.out.println("do you want a laptop");
+        Scanner scan = new Scanner(System.in);
+        String Ans1 = scan.next();
+
+        if (Ans1.equals("yes")) {
+            System.out.println("what is your favorite color between red-blue-yellow");
+        }
+        else {
+            System.out.println("no worries have a good day");
+        }
+
+    }
 }

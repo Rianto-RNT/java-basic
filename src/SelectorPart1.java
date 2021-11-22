2ea8b309e82b71f458f73087bcbd0c7ba0aac076
Rianto <rianto.rnt@gmail.com>
2021-11-22 18:33:53 +0800

    if Function in java

---
+++ src/SelectorPart1.java
@@ -0,0 +1,2 @@
+public class SelectorPart1 {
+}
---
+++ src/ifFunction.java
@@ -0,0 +1,20 @@
+public class ifFunction {
+    public static void main(String[] args) {
+
+        double a = 10;
+        double b = 10.1;
+        double c = 4;
+
+        if (b > a) {
+            System.out.println("B is greater than A");
+        }
+        else if (b < a) {
+            System.out.println("B is smaller than A");
+        }
+        else {
+            System.out.println("B is equal to A");
+        }
+    }
+}
+
+

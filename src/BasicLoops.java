6a4f909737730639ccc5357b927fb42bc176d308
Rianto <rianto.rnt@gmail.com>
2021-11-22 22:05:18 +0800

    BasicLoop

---
+++ src/BasicLoops.java
@@ -0,0 +1,12 @@
+public class BasicLoops {
+    public static void main(String[] args) {
+
+        int Num1 =  5;
+        double var1 = 3;
+
+        for (int i = 0; i < 15; i+=Num1) {
+            System.out.println(i);
+        }
+
+    }
+}

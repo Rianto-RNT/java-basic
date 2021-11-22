c5cfc5696a0c841f70f275023c4d346d077f6452
Rianto <rianto.rnt@gmail.com>
2021-11-22 22:36:54 +0800

    Find color a car

--- src/BasicLoops.java
+++ src/BasicLoops.java
@@ -1,10 +1,10 @@
 public class BasicLoops {
     public static void main(String[] args) {
 
-        int Num1 =  5;
+        int Num1 = 5;
         double var1 = 3;
 
-        for (int i = 0; i < 15; i+=Num1) {
+        for (int i = 0; i < 15; i += Num1) {
             System.out.println(i);
         }
 

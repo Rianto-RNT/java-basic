f5d50e328187bdc58427d0fa5cdd0793d44c0157
Rianto <rianto.rnt@gmail.com>
2021-11-23 20:00:40 +0800

    Final Project part 4

--- src/FinalProject.java
+++ src/FinalProject.java
@@ -36,7 +36,36 @@ public class FinalProject {
                 System.out.println("do you want something to drink? Yes or No :");
                 String ans3 = scan.next();
 
-                while (!ans3.equals("Yes") && !ans3.equals("No")) ;
+                while (!ans3.equals("Yes") && !ans3.equals("No")) {
+                    System.out.println("Wrong command. Please try again: ");
+                    ans3 = scan.next();
+                }
+
+                if (ans3.equals("Yes")) {
+                    System.out.println("What would like to drink: Coke or Juice ?");
+                    String ans4 = scan.next();
+
+                    while (!ans4.equals("Coke") && !ans4.equals("Juice")) {
+                        System.out.println("Sorry we don't have it! What would you like between: Coke or Juice? ");
+                        ans4 = scan.next();
+                    }
+
+                    if (ans4.equals("Coke") || ans4.equals("Juice")) {
+                        System.out.println("Great, You have ordered." + ans2 + "and" + ans4);
+                    }
+
+                    System.out.println("Please confirm your password");
+                    pass = scan.next();
+
+                    while (!pass.equals(pass1)) {
+                        System.out.println("This is not the right password. please try again.");
+                        pass = scan.next();
+                    }
+                    System.out.println("Thank You. Your order is in way.");
+
+
+                }
+
             }
 
 
